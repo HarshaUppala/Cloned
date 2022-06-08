92968795ae267b1903c97625818af0088b50803f
HarshaUppala <uhkuppala999@gmail.com>
2022-06-08 16:05:07 +0530

    commit

--- project09/EmpApp.py
+++ project09/EmpApp.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template, request,flash, redirect,url_for, jsonify, session 
-from pymysql import connections
+from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session 
 from flask import Response,send_file
-import os
-import boto3
-import rds_db as db
-from config import *
 
 app = Flask(__name__)
 
@@ -22,77 +17,37 @@ db_conn = connections.Connection(
 output = {}
 table = 'empdata'
 
+import rds_db as db
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('AddEmp.html')
 
-@app.route("/about", methods=['POST'])
-def about():
-    return render_template('#')
+@app.route('/')
+def index():
+    
+    return render_template('index.html')
 
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    ename = request.form['ename']
-    email = request.form['email']
-    ephno = request.form['ephno']
-    exp = request.form['exp']
-    apt = request.form['apt']
-    gdscore = request.form['gdscore']
-    hrscore = request.form['hrscore']
-    location = request.form['location']
-    emp_resume = request.files['emp_resume']
+@app.route('/insert',methods = ['post'])
+def insert():
+    
+    if request.method == 'POST':
+       ename = request.form['ename']
+       email = request.form['email']
+       ephno = request.form['ephno']
+       exp = request.form['exp']
+       apt = request.form['apt']
+       gdscore = request.form['gdscore']
+       hrscore = request.form['hrscore']
+       location = request.form['location']
+       emp_resume = request.files['emp_resume']
 
-    insert_sql = "INSERT INTO empdata VALUES (%s, %s, %s, %s, %s ,%s ,%s ,%s)"
-    cursor = db_conn.cursor()
-    details = db.get_details()
+        db.insert_details(ename,email,ephno,exp,apt,gdscore,hrscore,location)
+        details = db.get_details()
         print(details)
         for detail in details:
             var = detail
         return render_template('AddEmpOutput.html',var=var)
 
-    if emp_resume.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (ename,email, ephno, exp, apt,gdscore,hrscore,location))
-        db_conn.commit()
-        # Uplaod image file in S3 #
-        emp_resume_name_in_s3 = "emp-name-" + str(ename) + "_Resume"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_resume_name_in_s3, Body=emp_resume)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_resume_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('AddEmpOutput.html', name=ename)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-
-def get_details():
-    cur=conn.cursor()
-    cur.execute("SELECT *  FROM empdata")
-    details = cur.fetchall()
-    return details
+if __name__ == "__main__":
+    
+    app.run(debug=True)
---
+++ project09/rds_db.py
@@ -0,0 +1,22 @@
+import pymysql
+import aws_credentials as rds
+conn = pymysql.connect(
+        host= "emp-database.cruq67uutafa.ap-south-1.rds.amazonaws.com", #endpoint link
+        port = "3306", # 3306
+        user = "admin", # admin
+        password = "adminadmin", #adminadmin
+        db = "empdb", #test
+        
+        )
+
+
+def insert_details(name,email,comment,gender):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO empdata (ename,email,ephno,exp,apt,gdscore,hrscore,location) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (ename,email,ephno,exp,apt,gdscore,hrscore,location))
+    conn.commit()
+
+def get_details():
+    cur=conn.cursor()
+    cur.execute("SELECT *  FROM empdata")
+    details = cur.fetchall()
+    return details
