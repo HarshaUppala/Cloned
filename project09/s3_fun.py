676ea8ddd6ee9945c361c7cd884c1e9511a4e9ef
HarshaUppala <uhkuppala999@gmail.com>
2022-06-09 21:13:44 +0530

    commit

--- project09/s3_fun.py
+++ project09/s3_fun.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 BUCKET = "<dataemployee001>"
 
+
 def upload_file(file_name, bucket):
     object_name = file_name
     s3_client = boto3.client('s3')
@@ -17,7 +18,7 @@ def upload_file(file_name, bucket):
 
 def show_image(bucket):
     s3_client = boto3.client('s3')
-    public_urls = []
+    public_urls = ["https://s3{0}.amazonaws.com/{1}/{2}"]
     try:
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
             presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
