d9164ec3926da7a370fab0b6a52abcdbdaea6dd1
HarshaUppala <uhkuppala999@gmail.com>
2022-06-14 11:39:38 +0530

    commit

--- project09/templates/script.js
+++ project09/templates/script.js
@@ -1,3 +1,5 @@
-let apt=document.getElementById('aptid').value;
-document.getElementById("sal").innerHTML = apt;
-alert('hello');
+function process() {
+    let apt = document.getElementById('aptid').value;
+    document.getElementById("sal").innerHTML = apt;
+    alert('hello'); 
+}
